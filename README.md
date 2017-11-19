b9fb55a90c4f19a59180d56c4af559c424160a9d
evamlidon <evamarialidon@gmail.com>
2017-11-19 13:23:26 +0100

    1commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# chatBaloo
+Pure JS chatbot of Baloo, just for fun. 
+
+Demo https://temp-hosting.firebaseapp.com/
---
+++ public/script.js
@@ -0,0 +1,22 @@
+console.log('CARGADO');
+
+
+function handleSubmit() {
+  var element = document.getElementById('pc-input');
+  var value = element.value;
+  var pattern = /hola/;
+  var dialogTextElement = document.getElementById('dialog-text');
+
+  var dialog = [{
+  	
+  }]
+
+	if (pattern.test(value)) {
+	  dialogTextElement.innerText = 'Hola a ti';
+
+	} else {
+	  dialogTextElement.innerText = 'no se';
+	}
+
+  return false;
+}
